7f1ad6c29f38a13e783a7376568b3d5f0902b145
muammerkarasu <204567748+karasumuammer@users.noreply.github.com>
2025-04-05 17:04:50 +0300

    Kullanıcı Profil Yönetimi yapıldı

--- src/routes/StackRoutes.js
+++ src/routes/StackRoutes.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Start, Login, ForgotPassword, Register} from '../screens';
+import {Start, Login, ForgotPassword, Register, Home, Profile} from '../screens';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 const Tab = createBottomTabNavigator();
@@ -16,6 +16,8 @@ const StackRoutes = () => {
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Register" component={Register} />
       <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Profile" component={Profile} />
     </Stack.Navigator>
   );
 };
--- src/screens/index.js
+++ src/screens/index.js
@@ -3,5 +3,6 @@ import Start from './Start';
 import ForgotPassword from './ForgotPassword';
 import Register from './Register';
 import Home  from './Home';
+import Profile  from './Profile';
 
-export {Start, Login, ForgotPassword, Register,Home};
+export {Start, Login, ForgotPassword, Register,Home,Profile};
